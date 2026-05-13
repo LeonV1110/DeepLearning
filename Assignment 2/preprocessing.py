a222969488c1612300ecf901c4ee275d81575bc6
LeonV1110 <59734771+LeonV1110@users.noreply.github.com>
2026-05-13 14:51:52 +0200

    Initial implementation of preprocessing
    Currently still per file scaling, needs to be adjusted to be global scaling

---
+++ Assignment 2/preprocessing.py
@@ -0,0 +1,54 @@
+import h5py
+import pandas as pd
+import numpy as np
+from pathlib import Path
+from sklearn.preprocessing import StandardScaler
+
+INPUT = Path('data')
+DATATYPE = 'Cross' # or 'INTRA'
+DATATYPE = 'INTRA'
+OUTPUT = Path('preprocessed_data')
+def get_dataset_name(file_name_with_dir: Path):
+    
+    filename_without_dir = file_name_with_dir.name
+    temp = filename_without_dir.split('_')[:-1]
+    dataset_name = '_'.join(temp)
+    return dataset_name
+
+
+
+def preprocess_file(filename_path: Path, drop_out_factor = 10):
+    # Loading in the data
+    dataset_name = get_dataset_name(filename_path)
+    with h5py.File(filename_path, 'r') as f:
+        matrix = f.get(dataset_name)[()]
+    # Actual preprocessing
+    initial = pd.DataFrame(matrix)
+    dropped_out = initial.iloc[:, ::drop_out_factor]
+    scaled_and_transposed = pd.DataFrame(StandardScaler().fit_transform(dropped_out.T), 
+                                        columns=dropped_out.T.columns,
+                                        index = dropped_out.T.index)#TODO make scaler fit on all data, not just the current file
+    preprecossed = scaled_and_transposed.T
+    #storing the preprocessed data
+    path = (OUTPUT / filename_path.relative_to(INPUT)).with_suffix('.csv')
+    path.parent.mkdir(parents=True, exist_ok=True)
+    preprecossed.to_csv(path)
+
+
+def main():
+    scaler = StandardScaler()
+
+
+
+    for folder in (INPUT / Path(DATATYPE)).iterdir():
+        if folder == INPUT / Path(DATATYPE) / Path('.DS_Store'):
+            print(folder)
+            continue
+        for filepath in folder.iterdir():
+
+            print(filepath)
+            preprocess_file(filepath)
+
+
+if __name__ == '__main__':
+    main()
